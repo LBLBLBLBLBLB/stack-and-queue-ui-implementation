e56d7333765dd7f23c435688539ac91100fffc94
Levani Birchadze <levanbirchadze@gmail.com>
2023-06-29 12:28:56 +0400

    feat: add functionality

--- src/App.js
+++ src/App.js
@@ -1,33 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Button from './components/Button';
 
 const App = () => {
+  const [arrValues, setArrValues] = useState([]);
+  const [selected, setSelected] = useState('');
+
+  const handlePush = () => {
+    if (selected === '') {
+      return;
+    }
+    let tempArray = [...arrValues];
+    tempArray.push(tempArray.length + 1);
+    setArrValues(tempArray);
+  }
+
+  const handlePop = () => {
+    if (selected === '') {
+      return;
+    }
+    if (selected === 'stack') {
+      let tempArray = [...arrValues];
+      tempArray.pop();
+      setArrValues(tempArray);
+    } else {
+      let tempArray = [...arrValues];
+      tempArray.shift();
+      setArrValues(tempArray);
+    }
+  }
+
   return (
     <div className='mainContainer'>
       <div className='leftSide'>
         <div className='modeOption'>
           <Header header='Select Mode' />
           <div className='btnContainer'>
-            <Button text='Stack' />
+            <Button text='Stack' style={{ backgroundColor: selected === "stack" && "#3D9970" }} handleClick={() => setSelected('stack')} />
             <p className='or'>or</p>
-            <Button text='Queue' />
+            <Button text='Queue' style={{ backgroundColor: selected === "queue" && "#3D9970" }} handleClick={() => setSelected('queue')} />
           </div>
         </div>
         <div className='modeOption'>
           <Header header='Select Operation' />
           <div className='btnContainer'>
-            <Button text='Push' />
+            <Button text='Push' handleClick={handlePush} />
             <p className='or'>or</p>
-            <Button text='pop' />
+            <Button text='pop' handleClick={handlePop} />
           </div>
         </div>
       </div>
       <div className='stackQueue'>
-        <div className='part'>1</div>
-        <div className='part'>1</div>
-        <div className='part'>1</div>
+        {arrValues.map((val) => {
+          return <div className='part'>{val}</div>
+        })}
       </div>
     </div>
   )
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
 import styles from '../styles/Button.module.css';
 
 const Button = (props) => {
+
 	return (
 		<>
-			<button className={styles.button}>
+			<button className={styles.button} style={props.style} onClick={props.handleClick}>
 				{props.text}
 			</button>
 		</>
